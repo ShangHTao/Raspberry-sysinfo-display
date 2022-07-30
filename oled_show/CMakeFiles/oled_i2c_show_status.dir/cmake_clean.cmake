717ff0d206c317d913d3aa0f4179ba1fae3767a0
shanghongtao <541704813@qq.com>
2022-07-30 20:45:36 +0800

    all files

---
+++ oled_show/CMakeFiles/oled_i2c_show_status.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/oled_i2c_show_status.dir/src/main.c.o"
+  "CMakeFiles/oled_i2c_show_status.dir/src/ssd1306.c.o"
+  "CMakeFiles/oled_i2c_show_status.dir/src/sys_status.c.o"
+  "oled_i2c_show_status"
+  "oled_i2c_show_status.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/oled_i2c_show_status.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ oled_show/CMakeFiles/oledshwostatus.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/oledshwostatus.dir/src/main.c.o"
+  "CMakeFiles/oledshwostatus.dir/src/ssd1306.c.o"
+  "oledshwostatus"
+  "oledshwostatus.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/oledshwostatus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
